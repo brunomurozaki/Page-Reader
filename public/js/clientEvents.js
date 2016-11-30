675a45510839ceb17d03bd368c40524cb93bf991
Bruno Impossinato Murozaki <bruno.murozaki@intra.labone.net>
2016-11-30 13:13:46 -0200

    Dando uma melhorada na exp do usuario do prototipo

--- public/js/clientEvents.js
+++ public/js/clientEvents.js
@@ -7,6 +7,8 @@ function init(){
 	
 }
 
+enterStatus = 0;
+
 
 function configEvents(){
 	var address = $("#addressInput");
@@ -14,10 +16,16 @@ function configEvents(){
 	var stopButton = $("#stopButton");
 	var goButton = $("#goButton");
 	
-	address.on("keydown", onKeyPress);
+	$(document).on("keydown", onKeyPress);
+	address.on("focus", onAddressInputFocus);
 	readButton.on("click", onReadButtonClick);
 	stopButton.on("click", onStopButtonClick);
 	goButton.on("click", onGoButtonClick)
+	
+}
+
+function onAddressInputFocus(e){
+	enterStatus = 0;
 	
 }
 
@@ -25,7 +33,31 @@ function onKeyPress(e){
 	var key = e.which;
 	
 	if(key == 13){
-		callUrlPage($(this).val());
+		
+		switch(enterStatus){
+			case 0:
+			{
+				var val = $("#addressInput").val();
+				if(val.length > 0){
+					callUrlPage(val);
+					enterStatus++;
+				}
+				
+				break;
+			}
+			case 1: {
+				readContent();
+				enterStatus++;
+				break;
+			}
+			case 2: {
+				stopReading();
+				enterStatus = 0;
+				break;
+			}
+			default:
+		}
+
 		//$("#iframe").attr("src", $(this).val());
 	}
 }
@@ -38,13 +70,21 @@ function onGoButtonClick(e){
 	callUrlPage($("#addressInput").val());
 }
 
-function onStopButtonClick(e){
-	
-	responsiveVoice.cancel();
-}
 
-function onReadButtonClick(e){
+function readContent(){
 	var content = $(".content");
 	
 	responsiveVoice.speak(content.text(), "Brazilian Portuguese Female");
+}
+
+function stopReading(){
+	responsiveVoice.cancel();
+}
+
+function onStopButtonClick(e){
+	stopReading();
+}
+
+function onReadButtonClick(e){
+	readContent();
 }
